250786f1e2068bf73070729f0ba2ec8f98145ae7
al-yiyun <152640905+al-yiyun@users.noreply.github.com>
2025-10-10 10:49:11 -0400

    Fix docstring formatting in execute_command
    Updated docstring format for execute_command function.

--- fern/SoundSense.py
+++ fern/SoundSense.py
@@ -1,7 +1,9 @@
 import subprocess
 
 def execute_command():
-    ''' This function will try to open the entire program to begin testing'''
+    """
+        This function will try to open the entire program to begin testing
+    """
     try:
         result = subprocess.Popen('python gui_test.py', shell=True, text=True)
     except Exception as e:
