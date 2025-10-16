23dc58ed73a829f3e55edfd4e0c2349cd6a457f6
al-yiyun <152640905+al-yiyun@users.noreply.github.com>
2025-10-16 19:46:00 -0400

    Add docstring to execute_command function
    Added docstring to execute_command function.

--- ECE24-4/SoundSense.py
+++ ECE24-4/SoundSense.py
@@ -1,9 +1,13 @@
 import subprocess
 
 def execute_command():
+    """
+        This function will try to open the entire program to begin testing
+    """
     try:
         result = subprocess.Popen('python gui_app.py', shell=True, text=True)
     except Exception as e:
         print(e)
+
 
 execute_command()
