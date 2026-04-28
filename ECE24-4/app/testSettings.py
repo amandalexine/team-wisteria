c00a86b2688723da596d91873bb9b714061fe23f
Amanda-Lexine Sunga <152565370+amandalexine@users.noreply.github.com>
2026-04-28 12:40:39 -0400

    Create testSettings.py for test configuration
    Initialize testSettings.py with variables for test configuration and caching.

---
+++ ECE24-4/app/testSettings.py
@@ -0,0 +1,24 @@
+#Variables in this file are desinged to passed by refrence
+#Each section starts declared as None and is modified in the listed files
+#test_settings are imported as followed
+#import test_settings as set #stored variables passed by refrence between files
+
+
+#_______________Test Settings_______________
+#Wrote by test_sequence.py
+#Read by ProcResults.py
+Sampling_rate = None
+Channels_used = None 
+
+                       
+
+
+#_______________Cache for ProcResults_______________
+#Wrote by ProcResults.py
+#Read by ProcResults.py
+
+system_phase = None   #System phase 0 = No recordings, 1 = baseline recorded, 2 = baseline recorded and in test phase
+
+
+#baseline values
+
